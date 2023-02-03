1c84155bbb48b2ca4c2b56a41936df89c1587601
musabmemis <musabmemis@gmail.com>
2023-02-03 22:24:32 +0300

    javascript started files added

---
+++ 1-Baslangic/Javascript/Dersler/01-hello-world.js
@@ -0,0 +1,13 @@
+// ****************************** merhaba dünya örnekleri
+
+// dökümana(documet) yazdırma işlemi ile merhaba dünya:
+// document.write("Hello World")
+
+// Uyarıcı mesajı ile merhaba dünya:
+// alert("Merhaba Dünya")
+
+// Console.log ile Merhaba Dünya:
+console.log("Merhaba Dünya")
+console.log("Hayırlısı be gülüm")
+console.log("Hayırlısı be gülüm böyle gelmiş bu dünya böyle gider")
+
---
+++ 1-Baslangic/Javascript/Dersler/02-Degisken-tanimlama.js
@@ -0,0 +1,44 @@
+// ******** Let ve Const ile Değişken Tanımlama *******
+
+// Var ile değişken tanmlamak:
+
+// var serverName = "api.Kodluyoruz.org"
+// console.log(serverName)
+
+// let ile değişkeni boş tanımlamak:
+let serverName;
+console.log(serverName)
+// let ile değişkene bilgi atamak:
+serverName = "https://kodluyoruz.org"
+console.log(serverName)
+// let ile değişkene bilgi atayarak tanımlamak:
+let password = "1234"
+console.log(password)
+// değişken ataması yapmadan önce kullanmaya çalışmak:
+/* HATALI KULLANIM
+let fullName = "Musab Memiş"
+console.log(fullName)*/
+let fullName = "Musab Memiş";
+
+// let ile tanımlanan değişkenin içindeki bilgiyi değiştirmek:
+
+fullName = "lorem ipsum dolor"
+console.log(fullName)
+
+// birleştirme veya ekleme işlemi:
+fullName + "Yeni eklenen bilgi"
+console.log(fullName+ " Test bilgisi") // Ekle ve göster yaptık amadeğişkene eklemedik
+
+fullName= "Sıfırlandı"
+fullName+= " ve yeni bilgi eklendi"
+console.log(fullName)
+
+// const ile değişkeni boş tanımlamaya çalışmak :( :
+// const serverPassword; // Sadece değişkentanımlandı ama içi boş ??
+
+// const ile değişken tanımlamak:
+const SERVER_PASSWORD ="kshfdfjhldskj"
+console.log(SERVER_PASSWORD )
+
+// SERVER_PASSWORD = "1234"
+// console.log(SERVER_PASSWORD )
