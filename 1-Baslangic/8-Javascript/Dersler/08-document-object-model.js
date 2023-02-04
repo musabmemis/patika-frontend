93baf5f81a1771610e1476f6577fdf0fccdfe67c
musabmemis <musabmemis@gmail.com>
2023-02-04 20:03:21 +0300

    DOM

---
+++ 1-Baslangic/8-Javascript/Dersler/08-document-object-model.js
@@ -0,0 +1,9 @@
+// DOM
+// https://developer.mozilla.org/en-US/docs/Glossary/DOM
+// pathname
+console.log(document.location.pathname)
+console.log(document.body)
+
+
+document.body.style.backgroundColor="aqua"
+
