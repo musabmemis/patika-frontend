65988ae863cc20eb07fd7b4f13c7e8350158157b
musabmemis <musabmemis@gmail.com>
2023-02-06 19:39:45 +0300

    working on DOM

---
+++ 1-Baslangic/8-Javascript/Dersler/021-dom-etkinlikleri-ile-calismak.js
@@ -0,0 +1,9 @@
+// ********** DOM etkinlikleri ile çalışmak ********
+
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("click",domClick)
+
+function domClick (){
+    console.log("tıklandı")
+    this.style.color == "red" ?  this.style.color = "black" : this.style.color = "red"
+}
---
+++ 1-Baslangic/8-Javascript/Dersler/022-bolum-sonu-egzersizi.js
@@ -0,0 +1,38 @@
+// Functions Bolum Sonu Egzersizi
+
+let counter = 0
+
+let counterDOM = document.querySelector(`#counter`)
+let increaseDOM = document.querySelector(`#increase`)
+let decreaseDOM = document.querySelector(`#decrease`)
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click",clickEvent )
+decreaseDOM.addEventListener("click",clickEvent )
+
+// bu sekişle:
+
+// function clickEvent () {
+//     console.log(this.id)
+//     if (this.id == "increase") {
+//         counterDOM.innerHTML = counter += 1
+//     }else {
+//         counterDOM.innerHTML = counter -= 1
+//     }
+// }
+
+// ya da :
+
+// function clickEvent () {
+//     console.log(this.id)
+//     this.id == "increase" ? counterDOM.innerHTML = counter += 1 : counterDOM.innerHTML = counter -= 1
+// }
+
+// ya da :
+
+function clickEvent () {
+    console.log(this.id)
+    this.id == "increase" ? counter += 1  : counter -= 1
+    counterDOM.innerHTML = counter
+}
