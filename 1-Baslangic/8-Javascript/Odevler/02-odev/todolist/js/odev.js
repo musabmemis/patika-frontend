bc8a486e3cc22cccc921d99d9bcb54b8dc5c61d0
musabmemis <musabmemis@gmail.com>
2023-02-15 16:32:12 +0300

    odev update

--- 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
+++ 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
@@ -5,6 +5,26 @@ function listDone(event){
     event.target.classList.toggle("checked");
 }
 
+let myNodeList = document.getElementsByTagName("li")
+for (let i = 0; i < myNodeList.length; i++) {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+
 const idTask = document.querySelector("#task");
 
 function newElement() {
