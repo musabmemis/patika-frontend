f19613199d421df6e4ae733dd19431553980c780
musabmemis <musabmemis@gmail.com>
2023-02-16 02:30:19 +0300

    odev update

--- 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
+++ 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
@@ -7,36 +7,55 @@ function listDone(event) {
 
 let myNodeList = document.getElementsByTagName("li");
 for (let i = 0; i < myNodeList.length; i++) {
+  let btn = document.createElement("button");
   let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodeList[i].appendChild(span);
+  btn.type = "button";
+  btn.className = "close";
+  btn.ariaLabel = "Close";
+  span.ariaHidden = "true";
+  span.innerHTML = "&times";
+  btn.appendChild(span);
+  myNodeList[i].appendChild(btn);
 }
 
 let close = document.getElementsByClassName("close");
 
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function () {
-    let div = this.parentElement;
-    div.style.display = "none";
-  };
+    let div = close[i].parentElement;
+    div.remove()
 }
-
-const idTask = document.querySelector("#task");
+}
+function toastShow(str) {
+    $(document).ready(function () {
+      $(str).toast("show");
+    });
+  }
 
 function newElement() {
-  if (idTask.value) {
-    addItem(idTask.value);
-    idTask.value = "";
-    console.log("başarılı");
-  } else {
-    console.log("hatalı");
+    const idTask = document.querySelector("#task");
+    if (idTask.value) {
+      addItem(idTask.value);
+      idTask.value = "";
+      toastShow(".success");
+    } else {
+      toastShow(".error");
+    }
   }
-}
 
 const addItem = (idTask) => {
   let liDOM = document.createElement("li");
   liDOM.innerHTML = `${idTask}`;
   listDOM.append(liDOM);
+  for (let i = 0; i < myNodeList.length; i++) {
+    let btn = document.createElement("button");
+    let span = document.createElement("span");
+    btn.type = "button";
+    btn.className = "close";
+    btn.ariaLabel = "Close";
+    span.ariaHidden = "true";
+    span.innerHTML = "&times";
+    btn.appendChild(span);
+    myNodeList[i].appendChild(btn);
+  }
 };
