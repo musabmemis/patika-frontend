60513ef8d12f9b67ffd2bfff3990e6b46ca2ceb4
musabmemis <musabmemis@gmail.com>
2023-02-15 21:59:00 +0300

    code format

--- 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
+++ 1-Baslangic/8-Javascript/Odevler/02-odev/todolist/js/odev.js
@@ -1,44 +1,42 @@
-let listDOM = document.querySelector("ul#list") 
-listDOM.addEventListener("click",listDone);
+let listDOM = document.querySelector("ul#list");
+listDOM.addEventListener("click", listDone);
 
-function listDone(event){
-    event.target.classList.toggle("checked");
+function listDone(event) {
+  event.target.classList.toggle("checked");
 }
 
-let myNodeList = document.getElementsByTagName("li")
+let myNodeList = document.getElementsByTagName("li");
 for (let i = 0; i < myNodeList.length; i++) {
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodeList[i].appendChild(span);
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodeList[i].appendChild(span);
 }
 
 let close = document.getElementsByClassName("close");
 
 for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
 }
-
-
 
 const idTask = document.querySelector("#task");
 
 function newElement() {
-    if (idTask.value) {
-        addItem(idTask.value)
-        idTask.value = ""
-        console.log("başarılı")
-    }else { 
-        console.log("hatalı")
-    }
-} 
+  if (idTask.value) {
+    addItem(idTask.value);
+    idTask.value = "";
+    console.log("başarılı");
+  } else {
+    console.log("hatalı");
+  }
+}
 
 const addItem = (idTask) => {
-    let liDOM = document.createElement("li")
-    liDOM.innerHTML = `${idTask}`
-    listDOM.append(liDOM);
-}
+  let liDOM = document.createElement("li");
+  liDOM.innerHTML = `${idTask}`;
+  listDOM.append(liDOM);
+};
