5ba60b025036fdf167f8ac6c1f7e04b32fdd9ab5
musabmemis <musabmemis@gmail.com>
2023-03-23 16:14:29 +0300

    to do app update

--- 2-orta/3-React/odevler/odev2/to-do-app/src/App.js
+++ 2-orta/3-React/odevler/odev2/to-do-app/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <form>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autoFocus
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all" >Mark all as complete</label>
+
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn JavaScript</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn React</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Have a life!</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>2</strong>
+            items left
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a href="#/" className="selected">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/">Active</a>
+            </li>
+            <li>
+              <a href="#/">Completed</a>
+            </li>
+          </ul>
+
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+
+      <footer className="info">
+        <p>Click to edit a todo</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
     </div>
   );
 }
