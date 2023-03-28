4db78a91963b3d5b718be904fc9eea51bb0f5ba5
musabmemis <musabmemis@gmail.com>
2023-03-28 17:10:26 +0300

    update

---
+++ 2-orta/3-React/dersler/13-chatapp/clients/src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatForm from './ChatForm'
+import ChatList from './ChatList'
+
+function Container() {
+  return (
+    <div>
+        <ChatList/>
+        <ChatForm/>
+    </div>
+  )
+}
+
+export default Container
