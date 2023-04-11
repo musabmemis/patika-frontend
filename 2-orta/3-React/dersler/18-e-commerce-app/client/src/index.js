18a24a0afefdad6fd651ccfbf5f9e94a0c154677
musabmemis <musabmemis@gmail.com>
2023-04-11 08:13:49 +0300

    react query

--- 2-orta/3-React/dersler/18-e-commerce-app/client/src/index.js
+++ 2-orta/3-React/dersler/18-e-commerce-app/client/src/index.js
@@ -5,14 +5,19 @@ import "./reset.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
--- 2-orta/3-React/dersler/18-e-commerce-app/client/src/pages/Products.js
+++ 2-orta/3-React/dersler/18-e-commerce-app/client/src/pages/Products.js
@@ -1,7 +1,17 @@
 import Card from "../components/Card";
 import { Grid } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchProducts } from "../../api";
 
 function Products() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: fetchProducts,
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
   return (
     <div>
       <Grid templateColumns="repeat(4, 1fr)" gap={6}>
